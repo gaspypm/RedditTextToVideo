12af2060bfb48033cac5e6829cce6d0e746c6619
Gaspar Pérez Mazás <gaspypm@gmail.com>
2024-03-03 11:28:48 -0300

    Fetch posts titles

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello world")
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.reddit.com/r/AskReddit/'
+
+response = requests.get(url)
+
+html_content = response.text
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+posts = soup.find_all('article', class_='w-full m-0')
+
+posts_titles = []
+
+for post in posts:
+    post_title = post.find('a').text.strip()
+    posts_titles.append(post_title)
+    print(post_title)
